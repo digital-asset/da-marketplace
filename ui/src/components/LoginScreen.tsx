cde2c89fc25c39d89908872de4a7bc2e958eafaa
Dimitri Liakakos <dimitri.liakakos@digitalasset.com>
2020-09-02 18:25:46 +0300

    create user session contract. add dropdown

--- ui/src/components/LoginScreen.tsx
+++ ui/src/components/LoginScreen.tsx
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { useCallback } from 'react'
-import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image, Segment, Dropdown } from 'semantic-ui-react'
 import Credentials, { computeCredentials } from '../Credentials';
 import Ledger from '@daml/ledger';
 import { User } from '@daml.js/da-marketplace';
+import { Role } from "@daml.js/da-marketplace/lib/Marketplace"
 import { DeploymentMode, deploymentMode, ledgerId, httpBaseUrl} from '../config';
 import { useEffect } from 'react';
 
@@ -18,8 +19,9 @@ type Props = {
  */
 const LoginScreen: React.FC<Props> = ({onLogin}) => {
   const [username, setUsername] = React.useState('');
+  const [role, setRole] = React.useState('');
 
-  const login = useCallback(async (credentials: Credentials) => {
+  const login = useCallback(async (credentials: Credentials, role: string) => {
     try {
       const ledger = new Ledger({token: credentials.token, httpBaseUrl});
       let userContract = await ledger.fetchByKey(User.User, credentials.party);
@@ -27,6 +29,8 @@ const LoginScreen: React.FC<Props> = ({onLogin}) => {
         const user = {username: credentials.party, following: []};
         userContract = await ledger.create(User.User, user);
       }
+      const userSession = {operator: "Operator", user: credentials.party, role}
+      await ledger.create(Role.UserSession, userSession);
       onLogin(credentials);
     } catch(error) {
       alert(`Unknown error:\n${JSON.stringify(error)}`);
@@ -36,7 +40,7 @@ const LoginScreen: React.FC<Props> = ({onLogin}) => {
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
     const credentials = computeCredentials(username);
-    await login(credentials);
+    await login(credentials, role);
   }
 
   const handleDablLogin = () => {
@@ -55,15 +59,14 @@ const LoginScreen: React.FC<Props> = ({onLogin}) => {
     }
     url.search = '';
     window.history.replaceState(window.history.state, '', url.toString());
-    login({token, party, ledgerId});
-  }, [login]);
+    login({token, party, ledgerId}, role);
+  }, [login, role]);
 
   return (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as='h1' textAlign='center' size='huge' style={{color: '#223668'}}>
           <Header.Content>
-            Create
             <Image
               as='a'
               href='https://www.daml.com/'
@@ -74,7 +77,7 @@ const LoginScreen: React.FC<Props> = ({onLogin}) => {
               size='small'
               verticalAlign='middle'
             />
-            App
+            Marketplace App
           </Header.Content>
         </Header>
         <Form size='large' className='test-select-login-screen'>
@@ -91,6 +94,39 @@ const LoginScreen: React.FC<Props> = ({onLogin}) => {
                   className='test-select-username-field'
                   onChange={e => setUsername(e.currentTarget.value)}
                 />
+                <Segment>
+                <Dropdown
+                  placeholder='Choose your role'
+                  fluid
+                  selection
+                  options={[{
+                    key: 'operator',
+                    text: 'Operator',
+                    value: 'Operator',
+                  },
+                  {
+                    key: 'custodian',
+                    text: 'Custodian',
+                    value: 'Custodian',
+                  },
+                  {
+                    key: 'exchange',
+                    text: 'Exchange',
+                    value: 'Exchange',
+                  },
+                  {
+                    key: 'issuer',
+                    text: 'Issuer',
+                    value: 'Issuer',
+                  },
+                  {
+                    key: 'investor',
+                    text: 'Investor',
+                    value: 'Investor',
+                  }]}
+                  onChange={(_, data) => setRole(data.value as string)}
+                />
+                </Segment>
                 <Button
                   primary
                   fluid
