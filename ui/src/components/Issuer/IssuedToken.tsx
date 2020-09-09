89eb4d0669cc8d5cbb5e1d4606fedc3a826963f8
Sarah Breckenridge <sarah.breckenridge@digitalasset.com>
2020-09-09 17:13:51 -0400

    adding issuer role

---
+++ ui/src/components/Issuer/IssuedToken.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Header, Table } from 'semantic-ui-react'
+
+import { useStreamQuery } from '@daml/react'
+import { Token } from '@daml.js/da-marketplace/lib/Marketplace/Token'
+
+import './IssueAsset.css'
+
+const IssuedToken = () => {
+    const { tokenId } = useParams()
+
+    const token = useStreamQuery(Token).contracts.find(c => c.contractId === tokenId)
+
+    return (
+        <>
+            <Header as='h3'>{token?.payload.id.label}</Header>
+            <Header as='h3'>Trend Over Time</Header>
+            <Header as='h3'>Position Holdings</Header>
+            <Table basic='very' striped>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell></Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell></Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell></Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell></Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell></Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+        </>
+    )
+}
+
+export default IssuedToken;
---
+++ ui/src/components/Issuer/Issuer.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Page from '../common/Page'
+import WelcomeHeader from '../common/WelcomeHeader'
+
+import IssuerSideNav from './IssuerSideNav'
+import IssueAsset from './IssueAsset'
+import IssuedToken from './IssuedToken'
+
+type Props = {
+    onLogout: () => void;
+}
+
+const Issuer: React.FC<Props> = ({ onLogout }) => (
+    <Switch>
+        <Route path='/role/issuer/issue-asset'>
+            <Page sideNav={<IssuerSideNav/>} onLogout={onLogout}>
+                <WelcomeHeader/>
+                <IssueAsset/>
+            </Page>
+        </Route>
+
+        <Route path='/role/issuer/issued-token/:tokenId'>
+            <Page sideNav={<IssuerSideNav/>} onLogout={onLogout}>
+                <IssuedToken/>
+            </Page>
+        </Route>
+
+        <Route path='/'>
+            <Page sideNav={<IssuerSideNav/>} onLogout={onLogout}>
+                <WelcomeHeader/>
+            </Page>
+        </Route>
+    </Switch>
+)
+
+export default Issuer;
