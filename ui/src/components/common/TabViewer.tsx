07fb96b733104a2069463031377e53aaf6b5cdb1
Sarah Breckenridge <sarah.breckenridge@digitalasset.com>
2021-01-12 08:02:42 -0700

    add tab viewer

---
+++ ui/src/components/common/TabViewer.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+type TabViewerItem<T> = {
+    id: T;
+    label: string;
+}
+
+export type TabElementProps<T> = {
+    className: string;
+    children?: React.ReactElement;
+    itemId: T;
+}
+
+type Props<T> = {
+    className?: string;
+    currentId: T;
+    items: TabViewerItem<T>[];
+    hideTabs?: boolean;
+    Tab: (props: TabElementProps<T>) => JSX.Element;
+}
+
+
+type TabProps<T> = React.PropsWithChildren<Props<T>>
+
+const TabViewer = <T,>({ children, className, currentId, items, hideTabs, Tab }: TabProps<T>) : React.ReactElement => {
+    return (
+        <div className='tab-viewer'>
+            <div className='menu-entries'>
+                { items.filter(item => !hideTabs || item.id === currentId).map(item => Tab({
+                    className: `menu-entry ${'current' && currentId === item.id}`,
+                    children: <h5>{item.label}</h5>,
+                    itemId: item.id,
+                }))}
+            </div>
+
+            <div className={`content-body ${className}`}>
+                { children }
+            </div>
+        </div>
+    );
+}
+
+export default TabViewer;
