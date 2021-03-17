4f1b6d23a258543b45a673d1f97f79af406574ab
Alex Matson <alex.matson@digitalasset.com>
2021-03-17 06:30:19 -0400

    Update login screen (#234)

--- ui2/src/Main.tsx
+++ ui2/src/Main.tsx
@@ -5,7 +5,7 @@ import { useUserState, useUserDispatch } from "./context/UserContext";
 import Login from "./pages/login/Login";
 import Apps from "./Apps";
 import DamlLedger from "@daml/react";
-import { httpBaseUrl, wsBaseUrl } from "./config";
+import { httpBaseUrl } from "./config";
 import { Network } from "./apps/Network";
 import { Custody } from "./apps/Custody";
 import { Issuance } from "./apps/Issuance";
@@ -20,9 +20,9 @@ type MainProps = {
 
 export default function Main({ defaultPath }: MainProps) {
   const user = useUserState();
-  
+
   return (
-    <DamlLedger party={user.party} token={user.token} httpBaseUrl={httpBaseUrl} wsBaseUrl={wsBaseUrl}>
+    <DamlLedger party={user.party} token={user.token} httpBaseUrl={httpBaseUrl}>
       <HashRouter>
         <Switch>
           <Route exact path="/" component={RootRoute} />
