a8e824bea93b7ee2b68e4ee59b182b2c55d9f840
Alex Matson <alex.matson@digitalasset.com>
2021-05-05 11:55:16 -0400

    Format test file

--- ui2/src/App.test.tsx
+++ ui2/src/App.test.tsx
@@ -19,11 +19,15 @@ import './index.scss';
 const mockLedgerFunction = jest.fn();
 
 // See: https://discuss.daml.com/t/how-to-mock-usestreamqueries-in-jest-test/2095/2
-jest.mock('@daml/ledger', () => class {
-  streamQueries(...args: unknown[]): Stream<object, string, string, string[]>{
-    return {...mockLedgerFunction(...args), on: jest.fn(), close: jest.fn()};
-  }
-});
+jest.mock(
+  '@daml/ledger',
+  () =>
+    class {
+      streamQueries(...args: unknown[]): Stream<object, string, string, string[]> {
+        return { ...mockLedgerFunction(...args), on: jest.fn(), close: jest.fn() };
+      }
+    }
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
