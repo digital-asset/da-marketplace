8628392c0ff575d2b2611e4ca570e748bb721d2c
Alex Matson <alex.matson@digitalasset.com>
2021-05-06 10:04:54 -0400

    Use step and placeholder in listing form

--- ui2/src/util.ts
+++ ui2/src/util.ts
@@ -8,3 +8,16 @@ export function formatCurrency(amount: string | number, currency = 'USD', locale
 
   return formatter.format(value);
 }
+
+type PreciseInputSteps = {
+  step: string;
+  placeholder: string;
+};
+
+export function preciseInputSteps(precision: number): PreciseInputSteps {
+  const step = precision > 0 ? `0.${'0'.repeat(precision - 1)}1` : '1';
+
+  const placeholder = precision > 0 ? `0.${'0'.repeat(precision)}` : '0';
+
+  return { step, placeholder };
+}
