79480c44a4d0efa06f82078aca06e6de76a153dd
Alex Graham <alex.graham@digitalasset.com>
2021-05-18 15:58:18 -0400

    show failed and pending listings

--- ui2/src/pages/listing/Listings.tsx
+++ ui2/src/pages/listing/Listings.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { RouteComponentProps, useHistory, useParams, withRouter } from 'react-router-dom';
 import { CreateEvent } from '@daml/ledger';
 import { useParty, useStreamQueries } from '@daml/react';
-import { usePartyName } from '../../config';
-import { Service } from '@daml.js/da-marketplace/lib/Marketplace/Listing/Service';
+import { usePartyName, getTemplateId } from '../../config';
+import {
+  Service,
+  CreateListingRequest,
+  FailedListingCreation,
+} from '@daml.js/da-marketplace/lib/Marketplace/Listing/Service';
 import { Listing } from '@daml.js/da-marketplace/lib/Marketplace/Listing/Model';
 import Tile from '../../components/Tile/Tile';
 import { Listing as ListingComponent } from './Listing';
@@ -17,12 +21,18 @@ import { FairValueCalculationRequests } from './ManualCalculationRequests';
 import { ArrowRightIcon } from '../../icons/icons';
 import { ActionTile } from '../network/Actions';
 
+const FAILED_LISTING_TEMPLATE = 'Marketplace.Listing.Service.FailedListingCreation';
+const LISTING_REQUEST_TEMPLATE = 'Marketplace.Listing.Service.CreateListingRequest';
+
 type Props = {
   services: Readonly<CreateEvent<Service, any, any>[]>;
   listings: Readonly<CreateEvent<Listing, any, any>[]>;
 };
 
-export const getMarketType = (c: CreateEvent<Listing>, getName: (party: string) => string) => {
+export const getMarketType = (
+  c: CreateEvent<Listing | FailedListingCreation | CreateListingRequest>,
+  getName: (party: string) => string
+) => {
   const listingType = c.payload.listingType;
   if (listingType.tag === 'Collateralized') {
     return 'Collateralized';
@@ -43,46 +53,93 @@ export const ListingsTable: React.FC<Props> = ({ services, listings }) => {
   );
 
   const { contracts: fairValueContracts, loading: fairValuesLoading } = useStreamQueries(FairValue);
+  const { contracts: listingRequests, loading: listingRequestsLoading } =
+    useStreamQueries(CreateListingRequest);
+  const { contracts: failedListingRequests, loading: failedListingRequestsLoading } =
+    useStreamQueries(FailedListingCreation);
 
   return !contractId ? (
     <>
       <ActionTile actions={[{ path: '/app/setup/listing/new', label: 'New Listing' }]} />
-      <StripedTable
-        rowsClickable
-        headings={[
-          'Provider',
-          'Client',
-          'Cleared By',
-          'Listing ID',
-          'Calendar ID',
-          'Traded Asset',
-          'Traded Asset Precision',
-          'Quoted Asset',
-          'Quoted Asset Precision',
-          'Fair Value',
-        ]}
-        loading={fairValuesLoading}
-        rows={listings.map(c => {
-          const fairValues = fairValueContracts.filter(
-            fv => fv.payload.listingId.label === c.payload.listingId.label
-          );
-          return {
-            elements: [
-              getName(c.payload.provider),
-              getName(c.payload.customer),
-              getMarketType(c, getName),
-              c.payload.listingId.label,
-              c.payload.calendarId,
-              c.payload.tradedAssetId.label,
-              c.payload.tradedAssetPrecision,
-              c.payload.quotedAssetId.label,
-              c.payload.quotedAssetPrecision,
-              fairValues.length > 0 ? fairValues[fairValues.length - 1].payload.price : 'None',
-            ],
-            onClick: () => history.push('/app/manage/listings/' + c.contractId.replace('#', '_')),
-          };
-        })}
-      />
+      <Tile header={<h2>Listings</h2>}>
+        <StripedTable
+          rowsClickable
+          headings={[
+            'Provider',
+            'Client',
+            'Cleared By',
+            'Listing ID',
+            'Calendar ID',
+            'Traded Asset',
+            'Traded Asset Precision',
+            'Quoted Asset',
+            'Quoted Asset Precision',
+            'Fair Value',
+          ]}
+          loading={fairValuesLoading}
+          rows={listings.map(c => {
+            const fairValues = fairValueContracts.filter(
+              fv => fv.payload.listingId.label === c.payload.listingId.label
+            );
+            return {
+              elements: [
+                getName(c.payload.provider),
+                getName(c.payload.customer),
+                getMarketType(c, getName),
+                c.payload.listingId.label,
+                c.payload.calendarId,
+                c.payload.tradedAssetId.label,
+                c.payload.tradedAssetPrecision,
+                c.payload.quotedAssetId.label,
+                c.payload.quotedAssetPrecision,
+                fairValues.length > 0 ? fairValues[fairValues.length - 1].payload.price : 'None',
+              ],
+              onClick: () => history.push('/app/manage/listings/' + c.contractId.replace('#', '_')),
+            };
+          })}
+        />
+      </Tile>
+      {(!!listingRequests.length || !!failedListingRequests.length) && (
+        <Tile header={<h2>Requests</h2>}>
+          <StripedTable
+            headings={[
+              'Provider',
+              'Client',
+              'Cleared By',
+              'Listing ID',
+              'Calendar ID',
+              'Traded Asset',
+              'Traded Asset Precision',
+              'Quoted Asset',
+              'Quoted Asset Precision',
+              'Status',
+            ]}
+            loading={failedListingRequestsLoading || listingRequestsLoading}
+            rows={[...listingRequests, ...failedListingRequests].map(c => {
+              return {
+                elements: [
+                  getName(c.payload.provider),
+                  getName(c.payload.customer),
+                  getMarketType(c, getName),
+                  c.payload.symbol,
+                  c.payload.calendarId,
+                  c.payload.tradedAssetId.label,
+                  c.payload.tradedAssetPrecision,
+                  c.payload.quotedAssetId.label,
+                  c.payload.quotedAssetPrecision,
+                  getTemplateId(c.templateId) === LISTING_REQUEST_TEMPLATE ? 'Pending' : 'Failed',
+                  getTemplateId(c.templateId) === LISTING_REQUEST_TEMPLATE
+                    ? ''
+                    : (c.payload as FailedListingCreation).error.message
+                        .replace('{', '')
+                        .replace('}', '')
+                        .replace(/'/g, ''),
+                ],
+              };
+            })}
+          />
+        </Tile>
+      )}
       {!!manualFVRequests.length && (
         <Tile header={<h4>Manual Fair Requests</h4>}>
           <FairValueCalculationRequests
