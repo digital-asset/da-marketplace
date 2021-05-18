a75d88403fa534450601dc5f6863ef798e69abdb
Alex Matson <alex.matson@digitalasset.com>
2021-05-18 16:42:21 -0400

    delete ui

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,6 +1,15 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { VerifiedIdentity } from '@daml.js/da-marketplace/lib/Marketplace/Regulator/Model';
+import { partyNameFromJwtToken, useWellKnownParties } from '@daml/hub-react';
+import { Parties } from '@daml/hub-react/lib/WellKnownParties';
+import _ from 'lodash';
+import { useCallback, useMemo } from 'react';
+import { retrieveCredentials } from './Credentials';
+import { useStreamQueries } from './Main';
+import { retrieveParties } from './Parties';
+
 export enum DeploymentMode {
   DEV,
   PROD_DABL,
@@ -11,20 +20,88 @@ export const dablHostname = window.location.hostname.split('.').slice(1).join('.
 
 export const deploymentMode: DeploymentMode =
   process.env.NODE_ENV === 'development'
-  ? DeploymentMode.DEV
-  : dablHostname.includes('projectdabl')
-  ? DeploymentMode.PROD_DABL
-  : DeploymentMode.PROD_OTHER;
+    ? DeploymentMode.DEV
+    : dablHostname.includes('projectdabl')
+    ? DeploymentMode.PROD_DABL
+    : DeploymentMode.PROD_OTHER;
+
+export const isHubDeployment = deploymentMode === DeploymentMode.PROD_DABL;
 
 // Decide the ledger ID based on the deployment mode first,
 // then an environment variable, falling back on the sandbox ledger ID.
 export const ledgerId: string =
   deploymentMode === DeploymentMode.PROD_DABL
-  ? window.location.hostname.split('.')[0]
-  : process.env.REACT_APP_LEDGER_ID
-  ?? 'da-marketplace-sandbox';
+    ? window.location.hostname.split('.')[0]
+    : process.env.REACT_APP_LEDGER_ID ?? 'da-marketplace-sandbox';
 
 export const httpBaseUrl =
   deploymentMode === DeploymentMode.PROD_DABL
-  ? `https://api.${dablHostname}/data/${ledgerId}/`
-  : undefined;
+    ? `https://api.${dablHostname}/data/${ledgerId}/`
+    : undefined;
+
+export const wsBaseUrl = deploymentMode === DeploymentMode.DEV ? 'ws://localhost:7575/' : undefined;
+
+export const publicParty = deploymentMode === DeploymentMode.DEV ? 'Public' : `public-${ledgerId}`;
+
+const inferPartyName = (party: string, wellKnownParties: Parties | null): string | undefined => {
+  // Check if we can extract a readable party name from any data we have client-side.
+  // Inspect token claims, parties.json, and cross-reference Daml Hub's well-known parties.
+
+  const creds = retrieveCredentials();
+
+  if (party === creds?.party) {
+    return partyNameFromJwtToken(creds.token) || undefined;
+  }
+
+  if (party === wellKnownParties?.userAdminParty) {
+    return 'Operator';
+  }
+
+  if (party === wellKnownParties?.publicParty) {
+    return 'Public';
+  }
+
+  return retrieveParties()?.find(p => p.party === party)?.partyName;
+};
+
+export const usePartyName = (party: string) => {
+  const { contracts: verifiedIdentities, loading } = useStreamQueries(VerifiedIdentity);
+  const { parties: wellKnownParties } = useWellKnownParties();
+
+  const getName = useCallback(
+    (party: string): string => {
+      const legalName: string | undefined = verifiedIdentities.find(
+        id => id.payload.customer === party
+      )?.payload.legalName;
+
+      if (legalName) {
+        return legalName;
+      }
+
+      const inferredName: string | undefined = inferPartyName(party, wellKnownParties);
+
+      if (inferredName) {
+        return inferredName;
+      }
+
+      // Cannot find an identity contract or infer the party name, so fallback to party ID
+      return party;
+    },
+    [verifiedIdentities, loading]
+  );
+
+  const name = useMemo(() => getName(party), [party, getName]);
+
+  return { name, getName };
+};
+
+export const useVerifiedParties = () => {
+  const { contracts: identities, loading } = useStreamQueries(VerifiedIdentity);
+  const legalNames = _.uniq(identities.map(c => c.payload.legalName)).sort();
+  return { identities, legalNames, loading };
+};
+
+export function getTemplateId(t: string) {
+  const parts = t.split(':').slice(1);
+  return parts[0] + '.' + parts[1];
+}
--- ui/src/websocket/websocket.ts
+++ ui/src/websocket/websocket.ts
@@ -1,10 +1,10 @@
-import {useCallback, useEffect, useState} from 'react'
+import { useCallback, useEffect, useState } from 'react';
 
-import {ArchiveEvent, CreateEvent} from '@daml/ledger'
+import { ArchiveEvent, CreateEvent } from '@daml/ledger';
+import * as jtv from '@mojotech/json-type-validation';
 
-import {deploymentMode, DeploymentMode, httpBaseUrl, ledgerId} from '../config'
-
-import {ContractInfo, makeContractInfo} from '../components/common/damlTypes'
+import { deploymentMode, DeploymentMode, httpBaseUrl, ledgerId } from '../config';
+import { Template, ContractId, List, Text, Party } from '@daml/types';
 
 // A custom code to indicate that the websocket should not be reopened.
 // 4001 was picked arbitrarily from the range 4000-4999, which are codes that the official
@@ -21,151 +21,189 @@ const RETRY_TIME_INTERVAL = 5000;
 const TIME_UNTIL_INACTIVE = 10000;
 
 const newDamlWebsocket = (token: string): WebSocket => {
-  const url = new URL(httpBaseUrl || 'http://localhost:3000');
+  const url = new URL(httpBaseUrl || 'http://localhost:7575');
 
-  const subprotocols = [`jwt.token.${token}`, "daml.ws.auth"];
-  const apiUrl = deploymentMode === DeploymentMode.DEV
-    ? `ws://${url.host}/v1/stream/query`
-    : `wss://${url.host}/data/${ledgerId}/v1/stream/query`;
+  const subprotocols = [`jwt.token.${token}`, 'daml.ws.auth'];
+  const apiUrl =
+    deploymentMode === DeploymentMode.DEV
+      ? `ws://${url.host}/v1/stream/query`
+      : `wss://${url.host}/data/${ledgerId}/v1/stream/query`;
 
   return new WebSocket(apiUrl, subprotocols);
-}
+};
 
-function isCreateEvent<T extends object, K = unknown, I extends string = string>(event: object): event is { created: CreateEvent<T,K,I>} {
+function isCreateEvent<T extends object, K, I extends string = string>(
+  event: object
+): event is { created: CreateEvent<T, K, I> } {
   return 'created' in event;
 }
 
-function isArchiveEvent<T extends object, I extends string = string>(event: object): event is { archived: ArchiveEvent<T,I>} {
-    return 'archived' in event;
+function isArchiveEvent<T extends object, I extends string = string>(
+  event: object
+): event is { archived: ArchiveEvent<T, I> } {
+  return 'archived' in event;
 }
 
 function isWarningMessage(event: object): event is { warnings: object } {
-    return 'warnings' in event;
+  return 'warnings' in event;
 }
 
 export type StreamErrors = {
-    errors: string[];
-}
+  errors: string[];
+};
 
 function isErrorMessage(event: object): event is StreamErrors {
-    return 'errors' in event;
+  return 'errors' in event;
 }
 
-function useDamlStreamQuery(templateIds: string[], token?: string) {
-    const [ websocket, setWebsocket ] = useState<WebSocket | null>(null);
-    const [ contracts, setContracts ] = useState<ContractInfo<any>[]>([]);
-    const [ active, setActive ] = useState(false);
-    const [ loading, setLoading ] = useState(true);
-    const [ errors, setErrors ] = useState<StreamErrors>();
-    const [ timer, setTimer ] = useState<ReturnType<typeof setInterval>>(setInterval(() => clearInterval(timer), 1000));
-    const [ retries, setRetries ] = useState(0);
+// Decoder for a CreateEvent, copied from @daml/types
+const decodeCreateEvent = <T extends object, K, I extends string>(
+  template: Template<T, K, I>
+): jtv.Decoder<CreateEvent<T, K, I>> =>
+  jtv.object({
+    templateId: jtv.constant(template.templateId),
+    contractId: ContractId(template).decoder,
+    signatories: List(Party).decoder,
+    observers: List(Party).decoder,
+    agreementText: Text.decoder,
+    key: template.keyDecoder,
+    payload: template.decoder,
+  });
 
-    const messageHandlerScoped = useCallback(() => {
-        return (message: { data: string }) => {
-            if (timer != null) {
-                clearInterval(timer);
-                setTimer(setInterval(() => {
-                    setActive(false);
-                    clearInterval(timer);
-                }, TIME_UNTIL_INACTIVE));
-            }
-            setActive(true);
-            const data: { events: any[]; offset: string | undefined } = JSON.parse(message.data);
+function useDamlStreamQuery<T extends object, K, I extends string>(
+  templateIds: string[],
+  templateMap: Map<string, Template<T, K, I>>,
+  token?: string
+) {
+  const [websocket, setWebsocket] = useState<WebSocket | null>(null);
+  const [contracts, setContracts] = useState<CreateEvent<T, K, I>[]>([]);
+  const [active, setActive] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState<StreamErrors>();
+  const [timer, setTimer] = useState<ReturnType<typeof setInterval>>(
+    setInterval(() => clearInterval(timer), 1000)
+  );
+  const [retries, setRetries] = useState(0);
 
-            if (data.offset !== undefined) {
-                setLoading(false);
-            }
-
-            if (isWarningMessage(data)) {
-                console.warn("Warning emitted from DAML websocket: ", data.warnings);
-            }
-
-            if (isErrorMessage(data)) {
-                console.error(`Errors emitted from DAML websocket: ${data.errors}. Shutting down stream.`);
-                setErrors(data);
-            }
-
-            const { events } = data;
-
-            if (events && events.length > 0) {
-              events.forEach(e => {
-                if (isCreateEvent(e)) {
-                  const contract = makeContractInfo(e.created);
-                  if (!contracts.find(c => c.contractId === contract.contractId)) {
-                      setContracts(contracts => [...contracts, contract]);
-                  }
-                }
-
-                if (isArchiveEvent(e)) {
-                    const contractId = e.archived.contractId;
-                    if (contracts.find(c => c.contractId === contractId)) {
-                        setContracts(contracts => [...contracts.filter(c => c.contractId !== contractId)])
-                    }
-                }
-              })
-            }
+  const messageHandlerScoped = useCallback(
+    (templateMap: Map<string, Template<T, K, I>>) => {
+      return (message: { data: string }) => {
+        if (timer != null) {
+          clearInterval(timer);
+          setTimer(
+            setInterval(() => {
+              setActive(false);
+              clearInterval(timer);
+            }, TIME_UNTIL_INACTIVE)
+          );
         }
-    }, [contracts, loading]);
+        setActive(true);
+        setLoading(false);
 
-    const openWebsocket = useCallback(async (token: string, templateIds: string[]) => {
-        const ws = newDamlWebsocket(token);
+        const data: { events: any[]; offset: string | undefined } = JSON.parse(message.data);
 
-        ws.onopen = () => {
-            ws.send(JSON.stringify({templateIds}));
+        if (isWarningMessage(data)) {
+          console.warn('Warning emitted from DAML websocket: ', data.warnings);
         }
 
-        return ws;
-    }, []);
+        if (isErrorMessage(data)) {
+          console.error(
+            `Errors emitted from DAML websocket: ${data.errors}. Shutting down stream.`
+          );
+          setErrors(data);
+        }
 
-    const closeWebsocket = useCallback((token: string, templateIds: string[]) => {
-        return (event: CloseEvent) => {
-            // If this connection was closed unintentionally, set it to `null` to mark for the
-            // initialization effect hook to restart it.
-            if (event.code !== KEEP_CLOSED) {
-                setActive(false);
-                if (retries <= RETRIES_ALLOWED) {
-                    setRetries(retries + 1);
-                    setWebsocket(null);
-                } else {
-                    setTimeout(() => {
-                        setRetries(0);
-                        setWebsocket(null);
-                    }, RETRY_TIME_INTERVAL)
-                }
+        const { events } = data;
+
+        if (events && events.length > 0) {
+          events.forEach(e => {
+            if (isCreateEvent(e)) {
+              const template = templateMap.get(e.created.templateId);
+              if (!template) {
+                console.log(`could not find ${e.created.templateId}`);
+                return;
+              }
+              const contract = decodeCreateEvent(template).runWithException(e.created);
+              if (!contracts.find(c => c.contractId === contract.contractId)) {
+                setContracts(contracts => [...contracts, contract]);
+              }
             }
-        }
-    }, [openWebsocket, retries])
 
-    useEffect(() => {
-        // initialize websocket
-        if (!websocket && !errors && token && templateIds.length > 0) {
-            openWebsocket(token, templateIds).then(ws => {
-                setWebsocket(ws);
-            });
-        }
-
-        return () => {
-            if (websocket) {
-                clearInterval(timer);
-                websocket.close(KEEP_CLOSED);
-                setWebsocket(null);
+            if (isArchiveEvent(e)) {
+              const contractId = e.archived.contractId;
+              if (contracts.find(c => c.contractId === contractId)) {
+                setContracts(contracts => [...contracts.filter(c => c.contractId !== contractId)]);
+              }
             }
+          });
         }
-    }, [errors, token, templateIds, websocket, setWebsocket, openWebsocket])
+      };
+    },
+    [contracts, loading]
+  );
 
-    useEffect(() => {
-        if (websocket && token) {
-            websocket.onclose = closeWebsocket(token, templateIds);
+  const openWebsocket = useCallback(async (token: string, templateIds: string[]) => {
+    const ws = newDamlWebsocket(token);
+
+    ws.onopen = () => {
+      ws.send(JSON.stringify({ templateIds }));
+    };
+
+    return ws;
+  }, []);
+
+  const closeWebsocket = useCallback(
+    (token: string, templateIds: string[]) => {
+      return (event: CloseEvent) => {
+        // If this connection was closed unintentionally, set it to `null` to mark for the
+        // initialization effect hook to restart it.
+        if (event.code !== KEEP_CLOSED) {
+          setActive(false);
+          if (retries <= RETRIES_ALLOWED) {
+            setRetries(retries + 1);
+            setWebsocket(null);
+          } else {
+            setTimeout(() => {
+              setRetries(0);
+              setWebsocket(null);
+            }, RETRY_TIME_INTERVAL);
+          }
         }
-    }, [templateIds, token, websocket, closeWebsocket])
+      };
+    },
+    [openWebsocket, retries]
+  );
 
-    useEffect(() => {
-        if (websocket && token) {
-            websocket.onmessage = messageHandlerScoped();
-        }
-    }, [token, websocket, messageHandlerScoped])
+  useEffect(() => {
+    // initialize websocket
+    if (!websocket && !errors && token && templateIds.length > 0) {
+      openWebsocket(token, templateIds).then(ws => {
+        setWebsocket(ws);
+      });
+    }
 
-    return { contracts, errors, loading, active };
+    return () => {
+      if (websocket) {
+        clearInterval(timer);
+        websocket.close(KEEP_CLOSED);
+        setWebsocket(null);
+      }
+    };
+  }, [errors, token, templateIds, templateMap, websocket, setWebsocket, openWebsocket]);
+
+  useEffect(() => {
+    if (websocket && token) {
+      websocket.onclose = closeWebsocket(token, templateIds);
+    }
+  }, [templateIds, token, websocket, closeWebsocket]);
+
+  useEffect(() => {
+    if (websocket && token) {
+      websocket.onmessage = messageHandlerScoped(templateMap);
+    }
+  }, [token, websocket, templateMap, messageHandlerScoped]);
+
+  return { contracts, errors, loading, active };
 }
 
 export default useDamlStreamQuery;
