088b006508f7fd72726720c92c71c0ae43992c80
Sarah Breckenridge <sarah.breckenridge@digitalasset.com>
2021-05-20 07:51:02 -0400

    0.2.0;

--- exberry_adapter/setup.py
+++ exberry_adapter/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='da-marketplace-exberry-adapter',
-      version='0.0.2',
+      version='0.2.0',
       description='Daml Marketplace Exberry Adapter',
       author='Digital Asset',
       url='daml.com',
