891d029484c04887e5d29bf8b16751e4766c50e4
Alex Matson <alex.matson@digitalasset.com>
2021-05-21 13:26:07 -0400

    rename variable in tagger script

--- scripts/tag-versions.sh
+++ scripts/tag-versions.sh
@@ -13,9 +13,9 @@ vregex='[0-9]*\.[0-9]*\.[0-9]*(-[a-zA-Z]*\.[0-9]*)?'
 # daml.yaml files can't handle full semver versions, keep just the
 # MAJOR.MINOR.PATCH fields for them
 capture_regex="([0-9]*)\.([0-9]*)\.([0-9]*)(-[a-zA-Z]*\.[0-9]*)?"
-daml_version=$(echo $app_version | sed -r "s/^$capture_regex/\1.\2.\3/")
+short_version=$(echo $app_version | sed -r "s/^$capture_regex/\1.\2.\3/")
 
-echo "Application version is $app_version, Daml version is $daml_version."
+echo "Application version is $app_version, short version is $short_version."
 
 dabl_meta=$2
 daml_yaml=$3
@@ -36,31 +36,30 @@ else
 fi
 
 echo "  Tagging daml.yaml"
-yq w -i $daml_yaml 'version' "$daml_version"
+yq w -i $daml_yaml 'version' "$short_version"
 
 echo "  Tagging triggers daml.yaml"
-yq w -i $daml_yaml_triggers 'version' "$daml_version"
+yq w -i $daml_yaml_triggers 'version' "$short_version"
 yq w -i $daml_yaml_triggers 'data-dependencies' ""
-yq w -i $daml_yaml_triggers 'data-dependencies[+]' "../.daml/dist/da-marketplace-$daml_version.dar"
+yq w -i $daml_yaml_triggers 'data-dependencies[+]' "../.daml/dist/da-marketplace-$short_version.dar"
 
 echo "  Tagging integrationTesting daml.yaml"
-yq w -i $daml_yaml_it 'version' "$daml_version"
+yq w -i $daml_yaml_it 'version' "$short_version"
 yq w -i $daml_yaml_it 'data-dependencies' ""
-yq w -i $daml_yaml_it 'data-dependencies[+]' "../.daml/dist/da-marketplace-$daml_version.dar"
+yq w -i $daml_yaml_it 'data-dependencies[+]' "../.daml/dist/da-marketplace-$short_version.dar"
 
 echo "  Tagging exberry_adapter setup.py"
-sed -ri "s/version='$vregex/version='$daml_version/" $exberry_setup
+sed -ri "s/version='$vregex/version='$short_version/" $exberry_setup
 
 echo "  Tagging ui package.json"
 sed -ri "s/\"version\": \"$vregex/\"version\": \"$app_version/" $package_json
-sed -ri "s/da-marketplace\-$vregex\"/da-marketplace\-$daml_version\"/" $package_json
+sed -ri "s/da-marketplace\-$vregex\"/da-marketplace\-$short_version\"/" $package_json
 
 echo "  Tagging docs"
-sed -ri "s/da-marketplace\-$vregex/da-marketplace\-$daml_version/" $docs_localdev
+sed -ri "s/da-marketplace\-$vregex/da-marketplace\-$short_version/" $docs_localdev
 
-echo "HUHHHH $app_version $daml_version $docs_damlhub"
 sed -ri "s/da-marketplace\-$vregex.dit/da-marketplace\-$app_version.dit/" $docs_damlhub
-sed -ri "s/da-marketplace\-$vregex.dar/da-marketplace\-$daml_version.dar/" $docs_damlhub
-sed -ri "s/da-marketplace-exberry-adapter\-$vregex/da-marketplace-exberry-adapter\-$daml_version/" $docs_damlhub
+sed -ri "s/da-marketplace\-$vregex.dar/da-marketplace\-$short_version.dar/" $docs_damlhub
+sed -ri "s/da-marketplace-exberry-adapter\-$vregex/da-marketplace-exberry-adapter\-$short_version/" $docs_damlhub
 
 echo "Tagged all files... check results before committing"
