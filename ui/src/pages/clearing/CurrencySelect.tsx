d0a640c9bfa407bfe5a69b88abf77486624a8150
Alex Graham <alex.graham@digitalasset.com>
2021-05-24 11:33:26 -0400

    change currency to id in clearing calcs

---
+++ ui/src/pages/clearing/CurrencySelect.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Id } from '@daml.js/da-marketplace/lib/DA/Finance/Types';
+import { Form } from 'semantic-ui-react';
+import { AssetDescription } from '@daml.js/da-marketplace/lib/Marketplace/Issuance/AssetDescription';
+import { createDropdownProp } from '../common';
+import { CreateEvent } from '@daml/ledger';
+
+export const CurrencySelect: React.FC<{
+  setter: React.Dispatch<React.SetStateAction<Id | undefined>>;
+  assets: readonly CreateEvent<AssetDescription>[];
+  label: string;
+  selected?: string;
+  readOnly?: boolean;
+}> = ({ setter, assets, label, readOnly, selected }) => {
+  const assetOptions = assets.map(a => createDropdownProp(a.payload.assetId.label));
+  return (
+    <Form.Select
+      className="select"
+      label={label}
+      placeholder="Select..."
+      required
+      readOnly={readOnly}
+      disabled={readOnly}
+      value={assets.find(a => a.payload.assetId.label === selected)?.payload.assetId.label}
+      options={assetOptions}
+      onChange={(_, change) =>
+        setter(
+          assets.find(c => c.payload.assetId.label === (change.value as string))?.payload.assetId
+        )
+      }
+    />
+  );
+};
