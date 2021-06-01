9dd4f254047280266f439f0bab813de0b496d2ce
Sarah Breckenridge <sarah.breckenridge@digitalasset.com>
2021-06-01 11:07:16 -0400

    yarn format

--- ui/src/pages/custody/Account.tsx
+++ ui/src/pages/custody/Account.tsx
@@ -254,7 +254,7 @@ const AccountComponent: React.FC<RouteComponentProps & ServicePageProps<Service>
 
   return (
     <>
-      <BackButton prevPage='Wallet'/>
+      <BackButton prevPage="Wallet" />
       <InputDialog {...transferDialogProps} isModal />
       <InputDialog {...creditDialogProps} isModal />
       <div className="account">
