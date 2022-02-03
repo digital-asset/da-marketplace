d4eba6442187cb801111b56c37d847a6ee304de8
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-02-03 18:20:11 +0100

    add required tools

--- shell.nix
+++ shell.nix
@@ -8,5 +8,13 @@ in
 pkgs.mkShell {
   buildInputs = [
     pkgs.bash
+    pkgs.gnumake
+    pkgs.jq
+    pkgs.python37
+    pkgs.yapf
+    pkgs.pipenv
+    pkgs.yq-go
+    pkgs.nodejs
+    pkgs.yarn
   ];
 }
