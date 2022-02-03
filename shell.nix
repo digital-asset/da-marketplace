ec9f964433b5bb09715ec5fdddb37e7197434f73
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-02-03 18:05:47 +0100

    init nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+# Update nixpkgs with:
+# nix-shell -p niv --run "niv update"
+
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.bash
+  ];
+}
